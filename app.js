6b3a44cea03b3e30d0a5c6c72b4d4934222a6d4d
waleed dridi <thereal.waleeddridi@gmail.com>
2019-12-22 14:58:43 +0100

    add header cors

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
+const app = express();
 
-var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world !');
