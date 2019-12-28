c0103df3c92ca1bb13609fa91c0bbba7ac38dae1
Waleed Dridi <thereal.waleeddridi@gmail.com>
2019-12-29 00:19:21 +0100

    test the account model

---
+++ specs/mocks/account.mock.js
@@ -0,0 +1,12 @@
+const faker = require("faker");
+module.exports = ()=>{
+    return {
+        id: faker.random.uuid(),
+        email: faker.internet.email(),
+        password: faker.name.findName(),
+        activated: faker.random.boolean(),
+        reset_token: faker.random.alphaNumeric(),
+        strategy:  faker.random.arrayElement(['email', 'googlePlus', 'facebook', 'linkedIn', 'github'])
+    };
+}
+
