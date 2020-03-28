2d36531023bd3413d5fba73cec0e868450b53d78
Waleed Dridi <thereal.waleeddridi@gmail.com>
2020-03-28 23:58:15 +0100

    fix(structure): fix file structure

---
+++ src/config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  jwt: {
+    secret: 'B9D90628453938C578C7F826DE5E5BD2BCAC29E10C5526888384BA74FCEA563E',
+    slatRounds: 10,
+  },
+  sendGrid: {
+    apiKey: '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',
+    mail: 'dridi.walid@esprit.tn'
+  },
+};
